287435d0429c06dc4d8406fc7e02d6827029dca6
minhdev2005 <minh.dhb25dvcn052@stu.ptit.edu.vn>
2026-01-20 02:45:38 +0700

    update schema

---
+++ prisma/migrations/20260119194435_rename_password_changed_at_to_password_version/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `passwordChangedAt` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "passwordChangedAt",
+ADD COLUMN     "passwordVersions" INTEGER DEFAULT 0;
